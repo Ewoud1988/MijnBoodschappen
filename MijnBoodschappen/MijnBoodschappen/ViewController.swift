dae44d4cd1090957f8adcba099b7a37429eac41f
Ewoud Wortelboer <ew_1988@live.nl>
2021-07-09 23:06:44 +0200

    Project setup.

---
+++ MijnBoodschappen/MijnBoodschappen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MijnBoodschappen
+//
+//  Created by Ewoud Wortelboer on 09/07/2021.
+//  Copyright © 2021 Ewoud Wortelboer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
